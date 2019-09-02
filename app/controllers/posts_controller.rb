b4995cc138b19c0b39e661aff5d5efd8713d13a2
meixuanoh <ohmx93@gmail.com>
2019-09-02 17:26:55 +0800

    new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :destroy, :edit, :update]
+  before_action :set_post, only: [:show, :destroy, :edit, :update]
 
   def index
     @posts = Post.all
