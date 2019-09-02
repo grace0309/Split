87aff3b7188a1b50935203f771d6f1d98c88984d
mayong <hhmayong@gmail.com>
2019-09-02 18:54:38 +0800

    saved changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'deals/new'
   get 'deals/create'
   devise_for :users
-  root to: 'posts#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :transactions, only: [ :create, :new]
