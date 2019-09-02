339632339612b08cf9c6154924057cdce743cae4
Grace Cheung <cgrace0309@gmail.com>
2019-09-02 12:00:11 +0800

    models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_012650) do
+ActiveRecord::Schema.define(version: 2019_09_02_035109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "deals", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "store_name"
+    t.string "details"
+    t.string "discount"
+    t.bigint "category_id"
+    t.integer "quota"
+    t.integer "starting_contribution"
+    t.integer "total_contribution"
+    t.string "units"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.boolean "status", default: true
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.string "user_"
+    t.integer "contribution"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_transactions_on_post_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +68,14 @@ ActiveRecord::Schema.define(version: 2019_09_02_012650) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
+  add_foreign_key "transactions", "posts"
+  add_foreign_key "transactions", "users"
 end
