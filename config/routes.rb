431b1e3cead7b335fb786f734cc67946787dee7a
mayong <hhmayong@gmail.com>
2019-09-04 11:57:01 +0800

    chat seed created

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :transactions, only: [ :create, :new]
+    resources :messages, only: [ :index, :new, :create ]
   end
 
   resources :transactions, only: [ :show, :edit, :update, :destroy] do
