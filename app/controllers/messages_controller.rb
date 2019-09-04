abd8a42255680c65b8df3d3c3c2fad967c9fb0f7
mayong <hhmayong@gmail.com>
2019-09-04 12:21:04 +0800

    messages added to post

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
-  before_action :set_posts, only: [ :show, :new, :create ]
+  before_action :set_posts, only: [ :index, :show, :new, :create ]
   def index
     @messages = Message.all
+    @message = Message.new
   end
 
   def new
@@ -11,13 +12,23 @@ class MessagesController < ApplicationController
   def create
     @message.user_id = current_user.id
     @message = Message.new(messages_params)
-    @message.save
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to post_messages(@posts) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'messages/index' }
+        format.js  # <-- idem
+      end
+    end
   end
 
   private
 
   def set_posts
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
   end
 
   def messages_params
