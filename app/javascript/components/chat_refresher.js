f626103397d0e59de9f5163e0a1544326b2dcc89
Grace Cheung <cgrace0309@gmail.com>
2019-09-05 11:33:10 +0800

    halfway

--- app/javascript/components/chat_refresher.js
+++ app/javascript/components/chat_refresher.js
@@ -1,11 +1,17 @@
-const refresh = function() {
-  if (document.getElementById('messages')) {
-    location.reload()
-  }
+const refresh = function(content, displayed_box) {
+  const content = document.querySelectorAll('.message_content');
+  const displayed_box = document.getElementById('messages')
+  const displayed_mes = displayed_box.querySelectorAll('.show')
+  let i
+
+  for (i = (displayed_mes.length-1); i < messages.length; i++) {
+    const insert_content = content[i].dataset.content;
+    displayed_box.insertAdjacentHTML('beforeend', insert_content);
+}
 }
 
 const refreshtimer = () => {
-  setInterval(refresh, 1000);
+  setInterval(refresh(content,displayed_box), 3000);
 }
 
 export { refreshtimer }
