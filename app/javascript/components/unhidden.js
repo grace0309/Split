0b30e9332c38ca743e7b9d007a920659c1be31e3
Grace Cheung <cgrace0309@gmail.com>
2019-09-11 15:07:31 +0800

    fixed

--- app/javascript/components/unhidden.js
+++ app/javascript/components/unhidden.js
@@ -1,5 +1,5 @@
 function selectorunhide() {
-  if (document.querySelectorAll(".dash-icon")) {
+  if (document.querySelectorAll(".fa-times-circle")) {
     document.addEventListener("click", function(event) {
       if (event.target.classList[1] === "fa-times-circle") {
         let closebtn = event.target.parentElement.parentElement;
