d31a7573d71cbd762fada8a0d26917949323ec30
mayong <hhmayong@gmail.com>
2019-09-12 11:05:50 +0800

    ching js change

--- app/javascript/components/ching.js
+++ app/javascript/components/ching.js
@@ -2,10 +2,10 @@ const ching = function() {
   if (document.querySelector(".post-btn")) {
     const a = new Audio("https://res.cloudinary.com/dtsg1dlrd/video/upload/v1568109541/ching_hi8asp.wav");
     const btn = document.querySelector(".post-btn");
-    btn.addEventListener("click", (event) => {
+    btn.addEventListener("onclick", (event) => {
     a.play();
     });
-    btn.addEventListener("touchstart", (event) => {
+    btn.addEventListener("ontouchstart", (event) => {
     a.play();
     });
 
